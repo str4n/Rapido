2a1ab1cbc24434633f44b3672cffd0dcd9bce034
Kamil <Str4ngerGameOne@gmail.com>
2024-04-02 18:42:35 +0200

    Customers service init

---
+++ src/Services/Customers/Rapido.Services.Customers.Core/Extensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Rapido.Services.Customers.Core;
+
+public static class Extensions
+{
+    public static IServiceCollection AddCore(this IServiceCollection services, IConfiguration configuration)
+        => services;
+}
