eadfef03f6adf51d65691f01f754e7c037f44fb1
Kamil <Str4ngerGameOne@gmail.com>
2024-04-08 13:27:00 +0200

    Messaging projects

---
+++ src/Framework/Rapido.Framework.Messaging/Brokers/IMessageBroker.cs
@@ -0,0 +1,8 @@
+﻿using Rapido.Framework.Common.Abstractions;
+
+namespace Rapido.Framework.Messaging.Brokers;
+
+public interface IMessageBroker
+{
+    Task PublishAsync<T>(T message) where T : class, IMessage;
+}
