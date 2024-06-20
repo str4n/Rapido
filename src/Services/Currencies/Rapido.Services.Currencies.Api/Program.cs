1e813f705dd4815001a932d5bf33e5c93e905889
Kamil <Str4ngerGameOne@gmail.com>
2024-06-20 14:35:34 +0200

    Currencies service

---
+++ src/Services/Currencies/Rapido.Services.Currencies.Api/Program.cs
@@ -0,0 +1,29 @@
+using Rapido.Framework;
+using Rapido.Framework.Health.HealthChecks;
+using Rapido.Services.Currencies.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddFramework();
+
+builder.Services
+    .AddCore(builder.Configuration)
+    .AddHealth(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseHealth();
+
+app
+    .MapGet("/", (AppInfo appInfo) => appInfo)
+    .WithTags("API")
+    .WithName("Info");
+
+app
+    .MapGet("/ping", () => "pong")
+    .WithTags("API")
+    .WithName("Pong");
+
+app.UseFramework();
+
+app.Run();
